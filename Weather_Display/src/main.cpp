e1495f2b983f482dd77dda7eff2dd6f91792a81d
AndreasStgm <andreassteegmans@hotmail.com>
2024-09-14 18:06:07 +0200

    Display can now be switched between inside and outside data with the press of a button

--- Weather_Display/src/main.cpp
+++ Weather_Display/src/main.cpp
@@ -35,11 +35,15 @@ enum DisplayState
 // ===== Variable/Constant Declarations =====
 
 TFT_eSPI display = TFT_eSPI();
-WeatherSensorMessage incomingWeatherData;
-ButtonState switchButtonState = ButtonState::RELEASED;
-DisplayState outputDisplayState = DisplayState::WAITING;
 
-const uint8_t buttonDebounceTime = 50;
+bool isCurrentlyDisplayingOutside = true;
+WeatherSensorMessage lastOutsideWeatherData;
+WeatherSensorMessage lastInsideWeatherData;
+ButtonState switchButtonState = ButtonState::RELEASED;
+DisplayState outputDisplayState = DisplayState::UPDATE; // Initial state is set to update so after initialization the readings are displayed
+
+const uint8_t BUTTON_DEBOUNCE_TIME = 50;
+const uint8_t SWITCH_BUTTON_PIN = 35;
 
 // ===== Function Declarations =====
 
@@ -47,27 +51,50 @@ const uint8_t buttonDebounceTime = 50;
 void printStatusMessage(char *text, char *status, uint16_t color);
 // Displays the temperature and relative humidity for the in- or outside
 void displaySensorReadings(bool isOutside, float temperature, float relativeHumidity);
+// ISR for handling when the switch button is pressed
+void switchDisplayButtonISR();
+// Handles the transition between the possible states
+void stateHandler();
+
+// ===== Microcontroller Setup and Loop functions =====
 
 void setup()
 {
+  // Configure button as input and attach an intterupt service routing (ISR)
+  pinMode(SWITCH_BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(SWITCH_BUTTON_PIN), switchDisplayButtonISR, CHANGE);
+
+  // Start display, provide some delay for settling and clear
   display.init();
   display.setRotation(1);
   display.fillScreen(TFT_BLACK);
 
   display.setCursor(0, 4, 4);
   display.setTextColor(TFT_WHITE);
+  display.println("ESP Weather Station\n          by AndreasStgm\n==========================\n");
   display.print("Starting");
-  for (uint8_t i = 0; i < 3; i++)
+  for (uint8_t i = 0; i <= 3; i++)
   {
     delay(1000);
     display.print(".");
   }
   display.fillScreen(TFT_BLACK);
   display.setCursor(0, 4, 4);
+
+  // Show correctly functioning display
   printStatusMessage("Display: ", "OK", TFT_GREEN);
 
-  WiFi.mode(WIFI_MODE_STA);
-  if (esp_now_init() != ESP_OK) // Check if ESP_NOW started successfully
+  // Start WiFi module in station mode
+  if (WiFi.mode(WIFI_MODE_STA))
+  {
+    printStatusMessage("Wireless: ", "OK", TFT_GREEN);
+  }
+  else
+  {
+    printStatusMessage("Wireless: ", "FAILED", TFT_RED);
+  }
+  // Start ESP-Now
+  if (esp_now_init() != ESP_OK)
   {
     printStatusMessage("ESP-Now: ", "FAILED", TFT_RED);
   }
@@ -82,9 +109,9 @@ void setup()
 
 void loop()
 {
-  displaySensorReadings(true, 69.69, 69.69);
+  stateHandler();
 
-  delay(5000);
+  delay(1);
 }
 
 // ===== Function Definitions =====
@@ -127,4 +154,64 @@ void displaySensorReadings(bool isOutside, float temperature, float relativeHumi
   display.pushImage(0, 64, 64, 64, humidity);
   display.setCursor(64, 80, 6);
   display.print(relativeHumidity);
+}
+
+void switchDisplayButtonISR()
+{
+  // Assigning two longs between which the taken time in ms will be counted to debounce the button input
+  static unsigned long last_cycle_interrupt_time = 0;
+  unsigned long cycle_interrupt_time = millis();
+
+  // If the current time is longer than the required debounce time
+  if (cycle_interrupt_time - last_cycle_interrupt_time > BUTTON_DEBOUNCE_TIME)
+  {
+    // And the button was in a released state
+    if (switchButtonState == ButtonState::RELEASED)
+    {
+      // The button now is being pressed (detecting the edge from unpressed to pressed)
+      switchButtonState = ButtonState::PRESSED;
+    }
+    // And the button was in the pressed state
+    else if (switchButtonState == ButtonState::PRESSED)
+    {
+      // The button now has been released again after pressing (detecting the edge from pressed to unpressed)
+      switchButtonState = ButtonState ::RELEASED;
+
+      // Switch from displaying outside to inside or vice versa
+      isCurrentlyDisplayingOutside = !isCurrentlyDisplayingOutside;
+
+      // The display now has to be updated, so the state is set
+      outputDisplayState = DisplayState::UPDATE;
+    }
+  }
+
+  // The ISR is now completed, the current time is now the last time for the next time the ISR is triggered
+  last_cycle_interrupt_time = cycle_interrupt_time;
+}
+
+void stateHandler()
+{
+  switch (outputDisplayState)
+  {
+  case DisplayState::UPDATE:
+    // Determine if outside or inside data should be displayed
+    if (isCurrentlyDisplayingOutside)
+    {
+      // Display outside data
+      displaySensorReadings(isCurrentlyDisplayingOutside, lastOutsideWeatherData.temperature, lastOutsideWeatherData.relativeHumidity);
+    }
+    else
+    {
+      // Display inside data
+      displaySensorReadings(isCurrentlyDisplayingOutside, lastInsideWeatherData.temperature, lastInsideWeatherData.relativeHumidity);
+    }
+    // Complete the state by setting it back to waiting
+    outputDisplayState = DisplayState::WAITING;
+    break;
+  case DisplayState::WAITING:
+    break;
+  default:
+    printStatusMessage("State Handler: ", "ERROR", TFT_RED);
+    break;
+  }
 }
